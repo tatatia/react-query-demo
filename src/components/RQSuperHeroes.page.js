26761cab4ced5e3d32070d6753ef2d4023e12094
Tanya S <thetattianochka@gmail.com>
2022-04-14 16:21:17 +0200

    success and error callbacks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,18 +1,32 @@
 import {useQuery} from 'react-query';
 import axios from "axios";
 
-const fetchSuperHeroes =()=>{
+const fetchSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes')
 }
 
 export const RQSuperHeroesPage = () => {
-    const {isLoading, data, isError, error, isFetching, refetch} = useQuery('super-heroes', fetchSuperHeroes,{enabled:false})
+    const onSuccess = (data) => {
+        console.log('Perform side effect after data fetching', data)
+    }
+    const onError = (error) => {
+        console.log('Perform side effect after encountering error', error)
+    }
+
+    const {
+        isLoading,
+        data,
+        isError,
+        error,
+        isFetching,
+        refetch
+    } = useQuery('super-heroes', fetchSuperHeroes, {onSuccess, onError,})
 
     if (isLoading || isFetching) {
         return <h2>Loading...</h2>
     }
 
-    if (isError){
+    if (isError) {
         return <h2>{error.message}</h2>
     }
 
