2b345d5dfb5e62e070fafa188e91d774de094cc1
Tanya S <thetattianochka@gmail.com>
2022-04-20 08:48:06 +0200

    added mutations

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,11 @@
-import {useSuperHeroesData} from "../hooks/useSuperHeroesData";
+import {useAddSuperHeroData, useSuperHeroesData} from "../hooks/useSuperHeroesData";
 import {Link} from "react-router-dom";
+import{useState} from "react";
 
 export const RQSuperHeroesPage = () => {
+    const [name, setName] = useState('')
+    const [alterEgo, setAlterEgo] = useState('')
+
     const onSuccess = (data) => {
         console.log('Perform side effect after data fetching', data)
     }
@@ -9,7 +13,15 @@ export const RQSuperHeroesPage = () => {
         console.log('Perform side effect after encountering error', error)
     }
 
-    const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError())
+    const {isLoading, data, isError, error, isFetching, refetch} = useSuperHeroesData(onSuccess, onError())
+
+   const { mutate: addHero} = useAddSuperHeroData()
+
+    const handleAddHeroClick = () => {
+        console.log({name, alterEgo})
+        const hero = {name, alterEgo}
+        addHero(hero)
+    }
 
     if (isLoading || isFetching) {
         return <h2>Loading...</h2>
@@ -22,11 +34,16 @@ export const RQSuperHeroesPage = () => {
     return (
         <>
             <h2>RQ Super Heroes Page</h2>
+            <div>
+                <input type='text' value={name} onChange={(e) => setName(e.target.value)}/>
+                <input type='text' value={alterEgo} onChange={(e) => setAlterEgo(e.target.value)}/>
+              <button onClick={handleAddHeroClick}>Add Heroes</button>
+            </div>
             <button onClick={refetch}>Fetch heroes</button>
             {
                 data?.data.map((hero) => {
                     return <div key={hero.id}>
-                    <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+                        <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
                     </div>
                 })
             }
